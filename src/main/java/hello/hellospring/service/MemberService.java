e2e425a03f35356f5bb9fd4707ea4bb66d49c68a
ksb96 <ksebin96@gmail.com>
2023-04-14 15:59:11 +0900

    2023-04-14 16:00

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,33 +9,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 //@Service
 public class MemberService {
-    private final MemberRepository memberRepository;
 
-    @Autowired
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+  private final MemberRepository memberRepository;
+
+  @Autowired
+  public MemberService(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
+  }
 
 
-    public long join(Member member) { //회원가입
-        // 같은 이름이 있는 중복회원은 안된다.
-        validateDuplicatieMember(member); // 중복회원 검증
-        memberRepository.save(member);
-        return member.getId();
-    }
+  public long join(Member member) { //회원가입
+    validateDuplicatieMember(member); // 중복회원 검증
+    memberRepository.save(member);
+    return member.getId();
 
-    public List<Member> findMembers() { // 전체 회원 조회
-        return memberRepository.findAll();
-    }
+  }
 
-    public Optional<Member> findOne(Long memberId) {
-        return memberRepository.findById(memberId);
-    }
+  public List<Member> findMembers() { // 전체 회원 조회
+    return memberRepository.findAll();
+  }
 
-    private void validateDuplicatieMember(Member member) {
-        memberRepository.findByName(member.getName())
-                .ifPresent(m -> {
-                    throw new IllegalStateException("이미 존재하는 회원입니다.");
-                });
-    }
+  public Optional<Member> findOne(Long memberId) {
+    return memberRepository.findById(memberId);
+  }
+
+  private void validateDuplicatieMember(Member member) {
+    memberRepository.findByName(member.getName())
+        .ifPresent(m -> {
+          throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+  }
 }
